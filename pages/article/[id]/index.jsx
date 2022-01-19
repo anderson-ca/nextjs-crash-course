48ca9543db5455b418f19aabf3f1c8e6afbc45d9
anderson <anderson.d.cardoso92@gmail.com>
2022-01-19 09:00:12 -0600

    include server-side rendering

--- pages/article/[id]/index.jsx
+++ pages/article/[id]/index.jsx
@@ -1,15 +1,35 @@
-import {useRouter} from 'next/router';
+// import {useRouter} from 'next/router';
+import Link from 'next/link';
 
-const article = () => {
+const article = ({ article }) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const router = useRouter();
-    const {id} = router.query;
+    // const router = useRouter();
+    // const {id} = router.query;
 
     return (
-        <div>
-            <h1>article root {id}</h1>
-        </div>
+        <>
+            <h1>
+                {article.title}
+            </h1>
+            <p>
+                {article.body}
+            </p>
+            <br />
+            <Link href='/'>Go Back</Link>
+        </>
     )
+}
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
+
+    const article = await res.json();
+
+    return {
+        props: {
+            article
+        }
+    }
 }
 
 export default article
